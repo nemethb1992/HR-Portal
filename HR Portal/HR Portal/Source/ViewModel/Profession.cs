508de567ab98df343a01c9766cf53f9021f67807
Németh Balázs <39947176+nemethb1992@users.noreply.github.com>
2019-01-13 00:41:33 +0100

    commit

--- HR Portal/HR Portal/Source/ViewModel/Profession.cs
+++ HR Portal/HR Portal/Source/ViewModel/Profession.cs
@@ -35,7 +35,7 @@ namespace HR_Portal.Source.ViewModel
                 (!prof.ertesult.Equals(9999) ? "," + prof.ertesult.ToString() : "")
                 +")";
             System.Windows.MessageBox.Show(command);
-            //MySql.Execute(command);
+            MySql.Execute(command);
             //ModelFullApplicant udata = new ApplicantImplementation().GetFullApplicantByEmail(prof.email);
             //DirectoryInfo profession = new DirectoryInfo(Files.GetProfessionUrl() + prof.id);
             //DirectoryInfo newID = new DirectoryInfo(Files.GetApplicantUrl() + udata.id);
